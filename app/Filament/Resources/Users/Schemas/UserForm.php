00e91b6811593c4045a40d290a21a679ff271591
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-10-16 10:32:01 +0700

    usr crud done

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)) //dengan cara ini maka saat update, password tidak berubah
+                    ->dehydrated(fn ($state) => filled($state)) //dengan cara ini maka saat update, password tidak berubah
+                    ->required(fn (string $context): bool => $context === 'create'), //required ini hanya untuk create, bukan update
+            ]);
+    }
+}
