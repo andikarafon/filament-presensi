fe1bfae811a0c155c0e775d0212293a89e01ed5a
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-10-20 17:06:41 +0700

    Instal Livewire done

--- app/Filament/Resources/Schedules/Pages/ListSchedules.php
+++ app/Filament/Resources/Schedules/Pages/ListSchedules.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Schedules\Pages;
 use App\Filament\Resources\Schedules\ScheduleResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Action;
 
 class ListSchedules extends ListRecords
 {
@@ -13,6 +14,10 @@ class ListSchedules extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            //jika ingin menambahkan action button, dan sebagainya
+            Action::make('presensi')
+                ->url(route('presensi'))
+                ->color('warning'),
             CreateAction::make(),
         ];
     }
