d86dda2f2c2c4c614852ff8b8e797a172ddd1191
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-10-27 17:08:54 +0700

    50% store presensi

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Attendance;
 use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
@@ -26,5 +27,41 @@ class Presensi extends Component
             'insideRadius' => $this->insideRadius,
             'attendance' => $attendance
         ]);
-    }
+    } //end of  render
+
+
+    public function store()
+    {
+        $this->validate([
+            'latitude' => 'required',
+            'longitude' => 'required'
+        ]);
+
+        $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+
+        if ($schedule) {
+            $attendance = Attendance::where('user_id', Auth::user()->id)
+                            ->where('created_at', Carbon::today())->first();
+            
+            if (!$attendance) {
+                $attendance = Attendance::create([
+                    'user_id' => Auth::user()->id,
+                    'schedule_latitude' => $schedule->office->latitude,
+                    'schedule_longitude' => $schedule->office->longitude,
+                    'schedule_start_time' => $schedule->shift->start_time,
+                    'schedule_end_time' => $schedule->shift->end_time,
+                    'latitude' => $this->latitude,
+                    'longitude' => $this->longitude,
+                    'start_time' => Carbon::now()->toTimeString(),
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            } else {
+                $attendance->update([
+                    'end_latitude' => $this->latitude,
+                    'end_longitude' => $this->longitude,
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            }
+        }
+    } //end of store
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,12 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('presensi', Presensi::class)->name('presensi');
 });
 
+
+//agar ketika posisi tidak login, dan diakses langsung ke fitur maka diarahkan ke Login Form
+Route::get('/login', function() {
+    return redirect('admin/login');
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
